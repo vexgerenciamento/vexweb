426ef0bf7b9b925364a3bbb5d14d325acfe9d215
vex <vexstartup@gmail.com>
2022-04-14 17:14:16 -0300

    Cadastro funcionando, Limpando os campos depois de cadastrar

---
+++ autenticar.js
@@ -0,0 +1,11 @@
+var user_final = localStorage.getItem('valueText');
+var nome = localStorage.getItem('nome');
+var senha = localStorage.getItem('senha');
+if((user_final== null) || (user_final=="")){
+    window.location.href = 'pag_ini.html';
+} else if((senha==null) || (senha==null)){
+    window.location.href = 'pag_ini.html';
+}
+else if((nome==null) || (nome==null) ){
+    window.location.href = 'pag_ini.html';
+}
